6a0ec7052acffec88dc63ffcc8cbd8df78847168
Alberto de Jesús García Peña <71906889+albrto-garcia@users.noreply.github.com>
2024-08-13 14:06:21 -0400

    updates to the structure

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -22,16 +22,35 @@ document
     var asunto = document.getElementById("subject").value;
     var mensaje = document.getElementById("message").value;
 
+    modal_wrapper.classList.add("active");
+    if (e.classList.contains("success_btn")) {
+      s_modal.classList.add("active");
+      e_modal.classList.remove("active");
+    } else if (e.classList.contains("error_btn")) {
+      s_modal.classList.remove("active");
+      e_modal.classList.add("active");
+    }
+
     if (nombre === "" || email === "" || asunto === "" || mensaje === "") {
-      alert("Error: Todos los campos deben estar llenos.");
+      document.getElementById("popupMessage").textContent =
+        "Error: Todos los campos deben estar llenos.";
+      document.getElementById("popup").style.display = "flex";
       return;
     }
 
     var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(email)) {
-      alert("Error: Debe ingresar un formato de correo electrónico válido.");
+      document.getElementById("popupMessage").textContent =
+        "Error: Debe ingresar un formato de correo electrónico válido.";
+      document.getElementById("popup").style.display = "flex";
       return;
     }
 
-    alert("¡Hola " + nombre + "! Tu mensaje ha sido enviado con éxito.");
+    document.getElementById("popupMessage").textContent =
+      "¡Hola " + nombre + "! Tu mensaje ha sido enviado con éxito.";
+    document.getElementById("popup").style.display = "flex";
   });
+
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+}
