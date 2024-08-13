1458c3111cf572d3c60085ce1da6fc5c7bef79b1
Alberto de Jesús García Peña <albertogarciap18@gmail.com>
2024-08-13 01:08:37 -0400

    minor changes

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,5 +1,19 @@
+document.querySelectorAll("nav ul li a").forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute("href").substring(1);
+    const targetElement = document.getElementById(targetId);
+
+    window.scrollTo({
+      top: targetElement.offsetTop - 50,
+      behavior: "smooth",
+    });
+  });
+});
+
 document
-  .getElementById("contact_form")
+  .getElementById("contact-form")
   .addEventListener("submit", function (e) {
     e.preventDefault();
 
